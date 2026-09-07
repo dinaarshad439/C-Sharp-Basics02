785b651db36cb4085efede418b2d7423ae3e10c7
dinaarshad439 <dinaarshad439@gmail.com>
2026-09-07 21:53:33 +0300

    Add project files.

---
+++ C#Basics02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C_Basics02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
