95778c883be76edf0c44ac25e8d7635d379ff7c1
dinaarshad439 <dinaarshad439@gmail.com>
2026-09-08 17:43:16 +0300

    Question 1 answer

--- C#Basics02/Program.cs
+++ C#Basics02/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace C_Basics02
+﻿using System.Text;
+
+namespace C_Basics02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region (Q1)  Convert the title to uppercase and store it in a new variable
+
+            Console.WriteLine("Enter the Title : ");
+
+            string Title = Console.ReadLine();
+
+            string UpperTitle = Title.ToUpper();
+
+            Console.WriteLine($"Title : {Title}");
+            Console.WriteLine($"Upper Title : {UpperTitle}");
+
+
+            #endregion
+
+
         }
+
+        
     }
 }
