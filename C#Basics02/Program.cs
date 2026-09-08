df866631047fe60ec67034c02fec0dd05e74b8a1
dinaarshad439 <dinaarshad439@gmail.com>
2026-09-08 17:46:16 +0300

    Question 2 answer

--- C#Basics02/Program.cs
+++ C#Basics02/Program.cs
@@ -9,21 +9,33 @@ namespace C_Basics02
 
             #region (Q1)  Convert the title to uppercase and store it in a new variable
 
-            Console.WriteLine("Enter the Title : ");
+            //Console.WriteLine("Enter the Title : ");
 
-            string Title = Console.ReadLine();
+            //string Title = Console.ReadLine();
 
-            string UpperTitle = Title.ToUpper();
+            //string UpperTitle = Title.ToUpper();
 
-            Console.WriteLine($"Title : {Title}");
-            Console.WriteLine($"Upper Title : {UpperTitle}");
+            //Console.WriteLine($"Title : {Title}");
+            //Console.WriteLine($"Upper Title : {UpperTitle}");
 
 
             #endregion
 
 
+            #region (Q2)  Check if both strings reference have the same object in memory
+
+            string Name = "clean code";
+            string Name2 = "clean code";
+
+            // Compare object references
+            Console.WriteLine($"Did they Have the same address ? {object.ReferenceEquals(Name, Name2)}");
+
+            #endregion
+
+
+
         }
 
-        
+
     }
 }
